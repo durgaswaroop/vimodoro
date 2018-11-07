44a6cd5dc1b34681a1e60fa17fe9e339b7405797
durgaswaroop <durgaswaroop@gmail.com>
2018-11-08 02:00:13 +0530

    Add timieleft to statusline every second
    This runs and adds stuff the time to the statuline but keeps on adding it until we call StopPomodoro().

--- plugin/vimodoro.py
+++ plugin/vimodoro.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timedelta
 
+import vim
+
 
 start_time = None
 end_time = None
@@ -20,10 +22,19 @@ def stop_pomodoro():
 
 
 def get_time_remaining():
+    if end_time is None:
+        return ""
+
     current_time = datetime.now()
     time_left = end_time - current_time
     seconds = time_left.seconds
     minutes_left = seconds // 60
     seconds_left = seconds % 60
+    #print(str(minutes_left) + ':' + str(seconds_left))
     return str(minutes_left) + ':' + str(seconds_left)
 
+def set_status_line():
+    time_remaining = get_time_remaining()
+    #vim.command(f"let tr = {time_remaining}")
+    if time_remaining:
+        vim.command("set statusline +=\ " + get_time_remaining())
--- plugin/vimodoro.vim
+++ plugin/vimodoro.vim
@@ -1,10 +1,16 @@
 let vimodoro_path = escape(expand('<sfile>:p:h'), '\')
 let python_file_path = vimodoro_path . '/vimodoro.py'
-execute 'py3file' . python_file_path
-"py3file python_file_path
+execute 'py3file ' . python_file_path
 
 function! StartPomodoro()
 	python3 start_pomodoro()
+endfunction
+
+let timer = timer_start(1000, 'UpdateStatuBar', {'repeat':-1})
+function! UpdateStatuBar(timer)
+	"execute 'let &ro = &ro'
+	"execute 'set statusline +=hello'
+	python3 set_status_line()
 endfunction
 
 function! GetTimeRemaining()
